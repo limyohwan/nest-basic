5f07a32654dc70dee86652b515ceae7dc91b3d8f
Yohwan Lim <dyghks7102@naver.com>
2024-02-08 23:51:09 +0900

    feat: 메모리 객체 crud 완료

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,15 +11,32 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string) {
+    createBoard(createBoardDto: CreateBoardDto) {
+        // const title = createBoardDto.title;
+        // const {title, description} = createBoardDto;
+
         const board: Board = {
             id: uuid(),
-            title, // title: title => 타입명과 매개변수명이 동일하면 생략가능
-            description: description,
+            title: createBoardDto.title, // title: title => 타입명과 매개변수명이 동일하면 생략가능
+            description: createBoardDto.description,
             status: BoardStatus.PUBLIC
         }
 
         this.boards.push(board);
+        return board;
+    }
+
+    getBoardById(id: string): Board {
+        return this.boards.find((board) => board.id === id);
+    }
+
+    deleteBoard(id: string): void {
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
         return board;
     }
 }
