287dfc25368820fdc1aaf6ab5466ca06fd328de7
Yohwan Lim <dyghks7102@naver.com>
2024-03-13 23:34:49 +0900

    feat: 유저 회원가입 로직 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
+
+    @Post('/signup')
+    signup(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<void> {
+        return this.authService.signup(authCredentialDto);
+    }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { DataSource, Repository } from "typeorm";
+import { AuthCredentialDto } from "./dto/auth-credential.dto";
 import { User } from "./user.entity";
 
 @Injectable()
 export class UserRepository extends Repository<User> {
     constructor(dataSource: DataSource) {
         super(User, dataSource.createEntityManager());
+    }
+
+    async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+        const {username, password} = authCredentialDto;
+        const user = this.create({username, password});
+        
+        try {
+            await this.save(user);
+        } catch (error) {
+            if(error.code === '23505') {
+                throw new ConflictException("Existing username");
+            }
+
+            throw new InternalServerErrorException();
+        }
     }
 }
